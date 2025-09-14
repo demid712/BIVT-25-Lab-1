af534789e0e0f44a290ea407d6fb471563e7985a
demid <76248933+demid712@users.noreply.github.com>
2025-09-15 00:39:54 +0300

    Add files via upload

--- Lab1/Blue.cs
+++ Lab1/Blue.cs
@@ -1,4 +1,8 @@
-﻿namespace Lab1
+﻿using System.ComponentModel.Design;
+using System.Runtime.Intrinsics.X86;
+using System.Security.Cryptography;
+
+namespace Lab1
 {
     public class Blue
     {
@@ -7,7 +11,10 @@
             bool answer = false;
 
             // code here
-
+            if ((a>0 && b>0) || (b<0 && a<0))
+            {             
+                answer = true;
+            }
             // end
 
             return answer;
@@ -17,7 +24,10 @@
             bool answer = false;
 
             // code here
-
+            if ((int) d == d)
+            {
+                answer = true;
+            }
             // end
 
             return answer;
@@ -27,7 +37,10 @@
             bool answer = false;
 
             // code here
-
+            if (a % b == 0)
+            {
+                answer = true;
+            }
             // end
 
             return answer;
@@ -37,7 +50,10 @@
             double answer = 0;
 
             // code here
-
+            d=Math.Abs(d);
+            f=Math.Abs(f);
+            g=Math.Abs(g);
+            answer =Math.Max(d,Math.Max(f,g));
             // end
 
             return answer;
@@ -47,7 +63,18 @@
             double answer = 0;
 
             // code here
-
+            if (x<-1)
+            {
+                answer = x;
+            }
+            else if (x >= -1 && x <= 0)
+            {
+                answer = x + 1;
+            }
+            else if (x > 0)
+            {
+                answer = x + 1;
+            }
             // end
 
             return answer;
@@ -57,7 +84,10 @@
             bool answer = false;
 
             // code here
-
+            if (circleS*4/Math.PI <= squareS)
+            {
+                answer=true;
+            }
             // end
 
             return answer;
@@ -68,20 +98,26 @@
             int answer = 0;
 
             // code here
-
+            if (Math.Abs(d) < Math.Abs(f)) {if (d > 0) { answer = -1; } else { return answer; } }
+            else {if (d>0) { answer = 1; } else { return answer; } }
             // end
 
-            return answer;
+                    return answer;
         }
         public bool Task8(int a, int b, int c)
         {
             bool answer = false;
 
             // code here
-            
+            int a1 = a / 2;
+            int b2 = b / 2;
+            int c2= c / 2;
+            int cnt = a1 + b2 + c2;
+            if (cnt % 3 == 0 && cnt < 3) { answer = true; }
+            else { cnt += 1; if (cnt % 3 == 0 && cnt < 3) { answer = true; } }
             // end
 
-            return answer;
+                return answer;
         }
     }
 }
